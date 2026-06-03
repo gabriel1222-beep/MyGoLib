7ee4023472df24d030c58a5d23d5c13e075f3527
gabriel1222-beep <github8334@gmail.com>
2026-06-03 10:10:29 -0300

    Added getString,getRune,getByte

--- io/io.go
+++ io/io.go
@@ -13,3 +13,19 @@ func getString() (string,error){
 	}
 	return strings.TrimSpace(text),nil
 }
+func getByte() (byte,error){
+	reader := bufio.NewReader(os.Stdin)
+	b,err := reader.ReadByte()
+	if err != nil{
+		return byte(0),err
+	}
+	return b,nil
+}
+func getRune() (rune,error){
+	reader := bufio.NewReader(os.Stdin)
+	r,_,err := reader.ReadRune()
+	if err != nil{
+		return rune(0),err
+	}
+	return r,nil
+}
